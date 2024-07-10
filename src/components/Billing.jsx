08ab6416f06ab417f315167265c8b7f834efb147
Abhinav Kumar <masterabhinav2000@gmail.com>
2024-07-11 01:32:51 +0530

    files added

---
+++ src/components/Billing.jsx
@@ -0,0 +1,32 @@
+import { apple, bill, google } from "../assets";
+import styles, { layout } from "../style";
+
+const Billing = () => (
+  <section id="product" className={layout.sectionReverse}>
+    <div className={layout.sectionImgReverse}>
+      <img src={bill} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
+
+      {/* gradient start */}
+      <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
+      <div className="absolute z-[0] w-[50%] h-[50%] -left-1/2 bottom-0 rounded-full pink__gradient" />
+      {/* gradient end */}
+    </div>
+
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Easily control your <br className="sm:block hidden" /> billing &
+        invoicing
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+      Take Charge of Your Finances with Ease! Our platform lets you effortlessly control your billing and invoicing, ensuring you stay organized and on top of your payments
+      </p>
+
+      <div className="flex flex-row flex-wrap sm:mt-10 mt-6">
+        <img src={apple} alt="google_play" className="w-[128.86px] h-[42.05px] object-contain mr-5 cursor-pointer" />
+        <img src={google} alt="google_play" className="w-[144.17px] h-[43.08px] object-contain cursor-pointer" />
+      </div>
+    </div>
+  </section>
+);
+
+export default Billing;
